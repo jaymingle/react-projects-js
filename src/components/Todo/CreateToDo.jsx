3c9bc2a56da8f116eb8ed08e4b111071f185aa75
Jay Mingle <jkawudi@gmail.com>
2024-04-07 03:47:40 +0000

    Changes

--- src/components/Todo/CreateToDo.jsx
+++ src/components/Todo/CreateToDo.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import {Box, styled, TextField} from "@mui/material";
 
 const CreateToDo = () => {
+
+    const StyledBox = styled(Box)({
+        display: 'flex',
+        justifyContent: 'center'
+    })
+
     return (
-        <div>
-            Todo Input
-        </div>
+        <StyledBox>
+            <TextField sx={{width: "80%"}} id="standard-basic" placeholder="Enter Todo" variant="standard" />
+        </StyledBox>
     );
 };
 
