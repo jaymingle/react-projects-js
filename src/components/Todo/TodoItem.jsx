da26b6ed0a6f4afde669106ed259761b51dc9a0f
Jay Mingle <jkawudi@gmail.com>
2024-04-07 03:35:02 +0000

    Changes

--- src/components/Todo/TodoItem.jsx
+++ src/components/Todo/TodoItem.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
-import {Stack} from "@mui/material";
+import {AppBar, IconButton, Stack, Toolbar, Typography} from "@mui/material";
+import {Delete} from "@mui/icons-material";
 
 function TodoItem() {
 
 
   return (
     <Stack>
-        TodoItem
+        <AppBar position="static">
+            <Toolbar variant="dense">
+                <Typography variant="h6" color="inherit" component="div">
+                    Photos
+                </Typography>
+                <IconButton edge="start" color="inherit" aria-label="menu" sx={{ ml: 2 }}>
+                    <Delete />
+                </IconButton>
+            </Toolbar>
+        </AppBar>
     </Stack>
   )
 }
