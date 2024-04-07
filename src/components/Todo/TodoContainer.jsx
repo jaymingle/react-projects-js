7621846e92e4caf3765661a987e6bec21938e8df
Jay Mingle <jkawudi@gmail.com>
2024-04-07 04:09:41 +0000

    Changes

--- src/components/Todo/TodoContainer.jsx
+++ src/components/Todo/TodoContainer.jsx
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import CreateToDo from "./CreateToDo.jsx";
 import TodoLists from "./TodoLists.jsx";
+
+const TODO_LISTS = [
+    {
+        id: Date.now(),
+        todo: 'Clean Room',
+        isDone: false,
+    },
+    {
+        id: Date.now(),
+        todo: 'Go to Church',
+        isDone: false,
+    },
+    {
+        id: Date.now(),
+        todo: 'Cook',
+        isDone: false,
+    }
+]
 
 const TodoContainer = () => {
     return (
